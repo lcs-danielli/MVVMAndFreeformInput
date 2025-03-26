7a20e3e91c1b63ecdac172e333eef88cd7f1d616
Daniel Li <danli@lcs.on.ca>
2025-03-26 11:56:11 -0400

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by 李泽宇 on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
