355636f6ef9d3e9d6d782c3a59a1983b3d531388
Daniel Li <danli@lcs.on.ca>
2025-03-26 19:44:29 -0400

    Make negative base work

--- MVVMAndFreeformInput/Views/PowerView.swift
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -25,8 +25,15 @@ struct PowerView: View {
                 HStack(alignment: .center) {
                     HStack(alignment: .top) {
                         
-                        Text("\(power.base.formatted())")
-                            .font(.system(size: 96))
+                        if power.base >= 0 {
+                            //
+                            Text("\(power.base.formatted())")
+                                .font(.system(size: 96))
+                        } else {
+                            Text("(\(power.base.formatted()))")
+                                .font(.system(size: 96))
+                        }
+                        
                         
                         Text("\(power.exponent)")
                             .font(.system(size: 44))
